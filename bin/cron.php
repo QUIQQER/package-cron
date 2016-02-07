da0357fad169dfb3f785f96713cebccc216b42b4
Henning Leutz <leutz@pcsg.de>
2016-02-07 12:17:27 +0100

    response codes -> browser call

--- bin/cron.php
+++ bin/cron.php
@@ -4,9 +4,21 @@
  * External execution
  */
 
-
 define('QUIQQER_SYSTEM', true);
 require dirname(dirname(dirname(dirname(__FILE__)))) . "/header.php";
 
-$Cron = new \QUI\Cron\Manager();
-$Cron->execute();
+use \Symfony\Component\HttpFoundation\Response;
+
+$Cron     = new QUI\Cron\Manager();
+$Response = QUI::getGlobalResponse();
+
+try {
+    $Cron->execute();
+
+    $Response->setStatusCode(Response::HTTP_OK);
+    $Response->send();
+
+} catch (QUI\Exception $Exception) {
+    $Response->setStatusCode(Response::HTTP_SERVICE_UNAVAILABLE);
+    $Response->send();
+}
