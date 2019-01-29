cd240dc845d32be1677aa98782ea3a90e645e83c
Patrick Müller <p.mueller@pcsg.de>
2019-01-29 12:29:53 +0100

    fix: use -> instead of ::

--- src/QUI/Cron/QuiqqerCrons.php
+++ src/QUI/Cron/QuiqqerCrons.php
@@ -201,7 +201,7 @@ class QuiqqerCrons
     public static function calculateMediaFolderSizes($params, Manager $CronManager)
     {
         try {
-            $projects = QUI::getProjectManager()::getProjects(true);
+            $projects = QUI::getProjectManager()->getProjects(true);
         } catch (QUI\Exception $Exception) {
             QUI\System\Log::addError('Something went wrong getting all projects to calculate the media folder sizes');
             QUI\System\Log::writeException($Exception);
