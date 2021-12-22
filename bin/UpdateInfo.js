ef3678ecef3866237b56b1a3ed28f72e9d831c71
Henning Leutz <leutz@pcsg.de>
2021-12-22 13:20:52 +0100

    feat: quiqqer/quiqqer#971

--- bin/UpdateInfo.js
+++ bin/UpdateInfo.js
@@ -16,68 +16,81 @@ define('package/quiqqer/cron/bin/UpdateInfo', [
 ], function (QUI, QUIAjax, QUILocale, anime) {
     "use strict";
 
-    QUIAjax.post("package_quiqqer_cron_ajax_updateCheck", function (result) {
-        if (!result.length) {
-            return;
-        }
-
-        const Message = new Element("div", {
-            class : "updates-are-available",
-            role  : 'alert',
-            html  : '<div class="updates-are-available-text">' +
-                    '    <div class="updates-are-available-icon">' +
-                    '        <span class="fa fa-exclamation"></span>' +
-                    '    </div>' +
-                    '    <div>' +
-                    '        ' + QUILocale.get('quiqqer/cron', 'message.updates.available') +
-                    '    </div>' +
-                    '</div>',
-            styles: {
-                opacity: 0
+    setTimeout(function () {
+        QUIAjax.post("package_quiqqer_cron_ajax_updateCheck", function (result) {
+            if (!result.length) {
+                return;
             }
-        }).inject(document.body);
 
-        new Element('button', {
-            html  : QUILocale.get('quiqqer/cron', 'message.updates.available.button'),
-            events: {
-                click: function () {
-                    Message.destroy();
-
-                    require([
-                        'utils/Panels',
-                        'controls/packages/Panel'
-                    ], function (PanelUtils, SystemPanel) {
-                        PanelUtils.openPanelInTasks(
-                            new SystemPanel()
-                        );
-                    });
+            const Message = new Element("div", {
+                class : "updates-are-available",
+                role  : 'alert',
+                html  : '<div class="updates-are-available-text">' +
+                        '    <div class="updates-are-available-icon">' +
+                        '        <span class="fa fa-exclamation"></span>' +
+                        '    </div>' +
+                        '    <div>' +
+                        '        ' + QUILocale.get('quiqqer/cron', 'message.updates.available') +
+                        '    </div>' +
+                        '</div>',
+                styles: {
+                    opacity: 0
                 }
-            }
-        }).inject(Message);
+            }).inject(document.body);
 
-
-        anime({
-            targets : Message,
-            duration: 250,
-            opacity : 1,
-            top     : 30,
-            easing  : 'easeOutSine',
-            complete: function () {
+            const closeMessage = function () {
                 anime({
-                    delay   : 4000,
                     targets : Message,
-                    duration: 250,
+                    duration: 150,
                     opacity : 0,
-                    top     : 0,
+                    top     : 70,
                     easing  : 'easeOutSine',
                     complete: function () {
                         Message.destroy();
                     }
                 });
-            }
-        });
+            };
 
-    }, {
-        "package": "quiqqer/cron"
-    });
+            new Element('button', {
+                html  : QUILocale.get('quiqqer/cron', 'message.updates.available.button'),
+                events: {
+                    click: function () {
+                        closeMessage();
+
+                        require([
+                            'utils/Panels',
+                            'controls/packages/Panel'
+                        ], function (PanelUtils, SystemPanel) {
+                            PanelUtils.openPanelInTasks(
+                                new SystemPanel()
+                            );
+                        });
+                    }
+                }
+            }).inject(Message);
+
+            new Element('button', {
+                html   : QUILocale.get('quiqqer/cron', 'message.updates.available.cancel.button'),
+                'class': 'updates-are-available-cancel',
+                styles : {
+                    marginRight: 10
+                },
+                events : {
+                    click: closeMessage
+                }
+            }).inject(Message);
+
+            Message.setStyle('top', 70);
+
+            anime({
+                targets : Message,
+                duration: 250,
+                opacity : 1,
+                top     : 60,
+                easing  : 'easeOutSine'
+            });
+        }, {
+            "package": "quiqqer/cron"
+        });
+    }, 3000);
 });
