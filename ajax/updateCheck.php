76ef100317347d06ca7e1c1e44f2d1b49a447353
Henning Leutz <leutz@pcsg.de>
2023-07-24 10:43:44 +0200

    fix(quiqqer/qsetup#166): update check is also checked

--- ajax/updateCheck.php
+++ ajax/updateCheck.php
@@ -5,9 +5,17 @@
  *
  * @return array
  */
+
+use QUI\Cron\Manager;
+
 QUI::$Ajax->registerFunction(
     'package_quiqqer_cron_ajax_updateCheck',
     function () {
+        // only execute if quiqqer is completely set up
+        if (Manager::isQuiqqerInstallerExecuted() === false) {
+            return [];
+        }
+        
         return QUI\Cron\Update::getAvailableUpdates();
     },
     false,
