f9224a53ab0b9fbafa28c18516912f1fb5c0e059
Henning Leutz <leutz@pcsg.de>
2024-08-22 10:38:43 +0200

    style: code style

--- ajax/history/get.php
+++ ajax/history/get.php
@@ -11,11 +11,11 @@ QUI::$Ajax->registerFunction(
     'package_quiqqer_cron_ajax_history_get',
     function ($params) {
         $CronManager = new QUI\Cron\Manager();
-        $params      = json_decode($params, true);
+        $params = json_decode($params, true);
 
         return [
-            'page'  => (int)$params['page'],
-            'data'  => $CronManager->getHistoryList($params),
+            'page' => (int)$params['page'],
+            'data' => $CronManager->getHistoryList($params),
             'total' => $CronManager->getHistoryCount()
         ];
     },
