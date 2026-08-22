487a03a2f247410ee533fe619b23f0608c1e10a7
Henning <leutz@pcsg.de>
2026-08-22 11:06:15 +0200

    fix(cronservice): handle missing standard project
    Normalize the nullable Core return before resolving the vhost.
    
    Related: quiqqer/cron#51

--- src/QUI/Cron/CronService.php
+++ src/QUI/Cron/CronService.php
@@ -4,6 +4,7 @@ namespace QUI\Cron;
 
 use QUI;
 use QUI\Exception;
+use QUI\Projects\Project;
 use QUI\System\Log;
 
 use function curl_exec;
@@ -53,7 +54,7 @@ class CronService
         }
 
         // VHost Domain
-        $vhost = QUI::getProjectManager()->getStandard()->getVHost(true, true);
+        $vhost = $this->getVHost(QUI::getProjectManager()->getStandard());
 
         // Check if https should be used.
         if (str_starts_with($vhost, 'https://')) {
@@ -94,6 +95,11 @@ class CronService
         }
 
         $this->baseUrl = !empty($baseUrl) ? rtrim($baseUrl, '/') : 'https://cron.quiqqer.com';
+    }
+
+    private function getVHost(?Project $Project): string
+    {
+        return $Project?->getVHost(true, true) ?? '';
     }
 
     /**
